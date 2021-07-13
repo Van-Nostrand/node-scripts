5f838f06a1e2025539ce1dc5e4b45e7ae36d3f86
Van-Nostrand <dmdoull43@gmail.com>
2021-07-12 19:09:32 -0700

    added readme. got rid of dem dang tabs

--- rename.js
+++ rename.js
@@ -1,13 +1,13 @@
 const fs = require("fs")
 
 fs.readdir(__dirname, (err, files) => {
-	files.forEach(file => {
-		if (file.match(/'/)) {
-			let newname = file.split("'").join("");
-			fs.rename("./" + file, newname, err => {
-				if (err) throw err;
-				console.log("renamed ", file, " to ", newname);
-			});
-		}
-	});
+  files.forEach(file => {
+    if (file.match(/'/)) {
+      let newname = file.split("'").join("");
+      fs.rename("./" + file, newname, err => {
+        if (err) throw err;
+        console.log("renamed ", file, " to ", newname);
+      });
+    }
+  });
 });
